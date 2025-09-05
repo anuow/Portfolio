750b024035121bc657193f0afeda8dd5a876e49e
Anirudh <anirudh157290@gmail.com>
2025-09-05 18:34:26 +0530

    added animations in both mobile and desktop view

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,27 +1,103 @@
 import { FlipWords } from "./FlipWords";
+import { motion } from "motion/react";
 
 const HeroText = () => {
+    const words = ["Secure", "Modern", "Scalable"];
+    const variants = {
+        hidden: { opacity: 0, x: -56 },
+        visible: { opacity: 1, x: 0 },
+    }
   return (
     <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
         {/* Desktop View */}
         <div className="flex-col hidden md:flex c-space">
-            <h1 className="text-4xl font-medium">
+            <motion.h1 
+                className="text-4xl font-medium"
+                variants={variants}
+                initial="hidden"
+                animate="visible"
+                transition={{ delay: 1 }}
+            >
                 Hi im Anu
-            </h1>
+            </motion.h1>
             <div className="flex flex-col items-start">
-                <p className="text-5xl font-medium text-nuetral-300">
+                <motion.p 
+                    className="text-5xl font-medium text-nuetral-300"
+                    variants={variants}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{ delay: 1.2 }}
+                >
                     A Developer Dedicated To Crafting
-                </p>
-                    <div>
+                </motion.p>
+                    <motion.div 
+                        variants={variants}
+                        initial="hidden"
+                        animate="visible"
+                        transition={{ delay: 1.5 }}
+                    >
                         <FlipWords 
-                        words={["Secure", "Modern", "Scalable"]} 
+                        words={words} 
                         className="font-black text-white text-8xl"
                         />
-                    </div>
-                <p className="text-4xl font-medium text-neutral-300">Web Solutions</p>
+                    </motion.div>
+                <motion.p 
+                    className="text-4xl font-medium text-neutral-300"
+                    variants={variants}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{ delay: 1.8 }}
+                >
+                    Web Solutions
+                </motion.p>
             </div>
         </div>
+
+
         {/* Mobile View */}
+        <div className="flex flex-col space-y-6 md:hidden">
+            <motion.p 
+                className="text-4xl font-medium"
+                variants={variants}
+                initial="hidden"
+                animate="visible"
+                transition={{ delay: 1 }}
+            >
+                Hi Im Anu
+            </motion.p>
+            <div>
+                <motion.p 
+                    className="text-5xl font-black text-neutral-300"
+                    variants={variants}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{ delay: 1.2 }}
+                >
+                    Building
+                </motion.p>
+
+                <motion.div
+                    variants={variants}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{ delay: 1.5 }}
+                >
+                    <FlipWords 
+                    words={words} 
+                    className="font-bold text-white text-7xl"
+                    />
+                </motion.div>
+                <motion.p 
+                    className="text-4xl font-black text-neutral-300"
+                    variants={variants}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{ delay: 1.8 }}
+                >
+                    Web Applications
+                </motion.p>
+            </div>
+        </div>
 
     </div>
   );
