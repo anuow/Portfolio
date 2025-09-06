4b2442740a800a2b6566f86f0878a76578280e14
anu <anirudh157290@gmail.com>
2025-09-06 19:38:01 +0530

    Add Loader fallback and responsive Astronaut in Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,18 @@
 
-import { Canvas } from '@react-three/fiber';
-import { Astronaut } from "../components/Astronaut"
+import { Canvas, useFrame } from '@react-three/fiber';
+
 import HeroText from "../components/HeroText"
 import ParallaxBackground from "../components/ParallaxBackground"
-import { OrbitControls } from '@react-three/drei';
+import { useMediaQuery } from 'react-responsive';
+import { Astronaut } from '../components/Astronaut';
+import { easing } from 'maath';
+import { Float } from '@react-three/drei';
+import { Suspense } from 'react';
+import Loader from '../components/Loader';
+
 
 const Hero = () => {
+  const isMobile = useMediaQuery({ maxWidth: 853})
   return (
     <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
         <HeroText />
@@ -14,14 +21,30 @@ const Hero = () => {
             className="absolute inset-0"
             style={{ width: "100vw", height: "100vh"}}
         >
-            <Canvas>
-                <Astronaut />
-                <OrbitControls />
+            <Canvas camera={{position: [0, 1, 3] }}>
+              <Suspense fallback={<Loader />}>
+                <Float>
+                  <Astronaut 
+                    scale={isMobile && 0.23} 
+                    position={isMobile && [0, -1.5, 0]}
+                  />
+                </Float>
+
+                <Rig />
+              </Suspense>
             </Canvas>
 
         </figure>
     </section>
   )
+}
+
+function Rig() {
+  return useFrame((state, delta) =>{
+    easing.damp3(
+      state.camera.position,
+      [state.mouse.x / 10, 1 + state.mouse.y / 10, 3], 0.5, delta);
+  })
 }
 
 export default Hero
