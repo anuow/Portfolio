4ad3b4f4d3b8929c66979ae7d4c9800955508444
anu <anirudh157290@gmail.com>
2025-09-08 17:29:30 +0530

    create Frameworks.jsx to showcase skills with OrbitingCircles

---
+++ src/components/Frameworks.jsx
@@ -0,0 +1,45 @@
+import { OrbitingCircles } from "./OrbitingCircles";
+export function Frameworks() {
+    const skills = [
+        "c",
+        "python",
+        "sql",
+        "html5",
+        "css3",
+        "javascript",
+        "react",
+        "tailwindcss",
+        "vitejs",
+        "git",
+        "github",
+        "flutter",
+        "dart",
+        "blender",
+    
+    ]
+  return (
+    <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
+      <OrbitingCircles iconSize={40}>
+        {skills.map((skill, index) => (
+            <Icon key={index} src={`assets/logos/${skill}.svg`}/>
+        ))}
+      </OrbitingCircles>
+      <OrbitingCircles 
+        iconSize={25} 
+        radius={100} 
+        reverse 
+        speed={2}
+      >
+        {skills.reverse().map((skill, index) => (
+            <Icon key={index} src={`assets/logos/${skill}.svg`}/>
+        ))}
+        
+      </OrbitingCircles>
+    </div>
+  );
+}
+
+const Icon = ({ src }) => (
+    <img src={src} className="duration-200 rounded-sm hover:scale-110" />
+)
+
