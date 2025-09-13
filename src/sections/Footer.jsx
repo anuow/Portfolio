f04ed2e6dadb5199b317eabd2c7835a22f50fd32
anu <anirudh157290@gmail.com>
2025-09-13 17:08:00 +0530

    cleaned up Footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
 import { mySocials } from '../constants'
 
 const Footer = () => {
   return (
-    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
+    <section className="w-full flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
         <div className='mb-4 bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full' />
         <div className='flex gap-2'>
             <p>Terms & Condition</p>
