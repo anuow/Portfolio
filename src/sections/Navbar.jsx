3fb522e4548c1509cc7fd1fe8f8a711c63f371de
anu <anirudh157290@gmail.com>
2025-09-13 17:06:16 +0530

    clean up Navbar links and structure
    - Removed duplicate <ul> wrapper
    - Updated Home link to point to root ("/") instead of "#home"
    - Ensured proper nesting of <a> inside <li>

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -4,9 +4,9 @@ function Navigation() {
     return (
         
 
-        <ul className="nav-ul">
+        <ul  className="nav-ul">
             <li className="nav-li">
-                <a className="nav-link" href="#home">
+                <a className="nav-link" href="/">
                     Home
                 </a>
             </li>
